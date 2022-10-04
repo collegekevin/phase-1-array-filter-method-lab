fc1977164e4bf948955b78684f04c6dfb3c23116
Kevin Price <collegekevin@gmail.com>
2022-10-04 16:05:14 -0700

    commit tree 32a63fe88a20efb5a9dd16a73093660491493ab3

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+//const matchingDrivers = 'Bobby'; 
+
+//const matchedName = drivers.filter(findMatching(matchingDrivers));
+
+//const matchedInitials = drivers.filter(fuzzyMatch);
+
+//const matchedProperty = drivers.filter(matchName);
+
+function findMatching (drivers, matchingDriver){
+     return drivers.filter(driver => driver === matchingDriver)
+    
+}
+
+// function findMatching (drivers){
+//     const foundDrivers = drivers === matchingDrivers;
+//     console.log(foundDrivers);
+//     return foundDrivers;
+// }
+
+function fuzzyMatch(drivers, namesBeginWith){
+    return drivers.filter(driver => driver.charAt(0) === namesBeginWith)
+}
+
+function matchName (drivers, string){
+    return drivers.filter(driver => driver === string)
+}
